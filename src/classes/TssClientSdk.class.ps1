5fcf02e89e84527007bc839d678082fa504cd7f4
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-02-12 18:46:01 -0600

    Testing

---
+++ src/classes/TssClientSdk.class.ps1
@@ -0,0 +1,17 @@
+﻿using namespace Thycotic.SecretServer.Sdk.Extensions.Integration.Clients;
+using namespace Thycotic.SecretServer.Sdk.Extensions.Integration.Models;
+using namespace Thycotic.SecretServer.Sdk.Infrastructure.Models;
+
+$config = [Thycotic.SecretServer.Sdk.Extensions.Integration.Models.ConfigSettings]::new()
+$config.RuleName = 'tssmodule'
+$config.CacheAge = 30
+$config.CacheStrategy = 0
+$config.SecretServerUrl = 'http://vault3'
+
+$client = [Thycotic.SecretServer.Sdk.Extensions.Integration.Clients.SecretServerClient]::new()
+$client.Configure($config,$true)
+
+$client.Configure()
+
+$s = New-TssSession -SecretServer 'http://vault3' -AccessToken $client.GetAccessToken()
+Get-TssSecret -TssSession $s -Id 19
